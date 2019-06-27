2afa9ecde8bf8c04de3779c4fc1342529470b178
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-06-27 22:29:41 +0500

    code optimised

--- src/containers/home/styles.js
+++ src/containers/home/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Grid = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr;
